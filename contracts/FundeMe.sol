8f2b103f0a64e04fbab74a962b014194c4b526c0
paliop <poli.rubin.17@gmail.com>
2024-11-08 22:46:03 -0300

    ult fund

--- contracts/FundeMe.sol
+++ contracts/FundeMe.sol
@@ -5,7 +5,16 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe{
     mapping (address=>uint256) public addressToAmountFounded;
+    
+    address public owner;
+
+    constructor()public {
+        owner= msg.sender;   /* para hacer que owner sea la adress del contrato */
+    }
+
     function fund()public payable{
+        uint256 minimumUSD = 50 * 10 **18;  /* min expresado en Gwei */
+        require(getConversionRate(msg.value) >= minimumUSD, "Higher Ammount Needed");
         addressToAmountFounded[msg.sender] += msg.value;
     }
     function getVersion() public view returns(uint256){
@@ -17,5 +26,19 @@ contract FundMe{
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
         (,int256 answer,,,)= priceFeed.latestRoundData(); /* todos los datos devueltos scado del contrato de linkl, pero solo se usa el que va usar, para el resto , y espacio vacion */
         return uint256(answer * 10000000000);  /* retorna solo el precio(answer) */
+    }
+    function getConversionRate (uint256 btcAmount) public view returns (uint256){
+        uint256 btcPrice= getPrice();
+        uint256 btcAmountInUsd = (btcPrice * btcAmount);
+        return btcAmountInUsd;
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _; /* _; esto indica donde se ejecuta resto del codigo, puede ir arriba tambien */
+    }
+
+    function withdraw() payable onlyOwner public{
+        msg.sender.transfer( address  (this).balance);
     }
 }
