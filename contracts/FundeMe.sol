b9056ccbebd0c905f0dbd04f7c03c8989e857948
paliop <poli.rubin.17@gmail.com>
2024-11-14 13:05:56 -0300

    ult fund2

--- contracts/FundeMe.sol
+++ contracts/FundeMe.sol
@@ -6,6 +6,8 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 contract FundMe{
     mapping (address=>uint256) public addressToAmountFounded;
     
+     address[] public funders; /*  array con personas que donaron */
+
     address public owner;
 
     constructor()public {
@@ -16,6 +18,7 @@ contract FundMe{
         uint256 minimumUSD = 50 * 10 **18;  /* min expresado en Gwei */
         require(getConversionRate(msg.value) >= minimumUSD, "Higher Ammount Needed");
         addressToAmountFounded[msg.sender] += msg.value;
+        funders.push(msg.sender); /*  pushea al aarray de fundadores */
     }
     function getVersion() public view returns(uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);  /* adress del feed sacado de chainlink */
